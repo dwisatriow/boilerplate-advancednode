c2037d8366aba2b8fc7568cb8ae0fe69ae0e8a22
dwisatriow <dwisatrioow@gmail.com>
2022-01-27 14:28:10 +0700

    Implement password hashing

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -61,13 +62,15 @@ myDB(async (client) => {
 
   app.route('/register')
     .post((req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
+
       myDatabase.findOne({ username: req.body.username }, function(err, user) {
         if (err) {
           next(err);
         } else if (user) {
           res.redirect('/')
         } else {
-          myDatabase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+          myDatabase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
             if (err) {
               res.redirect('/');
             } else {
@@ -107,7 +110,9 @@ myDB(async (client) => {
         console.log('User ' + username + ' attempted to login.');
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
         return done(null, user);
       });
     }
