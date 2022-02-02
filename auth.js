b6302abf6343910c60c4e6a4c43029da6478485e
dwisatriow <dwisatrioow@gmail.com>
2022-02-02 15:01:40 +0700

    Implement socoal authentication
    Implement authentication with github oauth and add prettier config

--- auth.js
+++ auth.js
@@ -1,31 +1,73 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const ObjectID = require('mongodb').ObjectID;
-const bcrypt = require('bcrypt');
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const GithubStrategy = require("passport-github2").Strategy;
+const ObjectID = require("mongodb").ObjectID;
+const bcrypt = require("bcrypt");
 
 module.exports = function (app, myDatabase) {
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
-      });
-    
-      passport.deserializeUser((id, done) => {
-        myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-          done(null, doc);
-        });
-      });
-    
-      passport.use(new LocalStrategy(
-        function(username, password, done) {
-          myDatabase.findOne({ username: username }, function(err, user) {
-            console.log('User ' + username + ' attempted to login.');
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (!bcrypt.compareSync(password, user.password)) {
-              return done(null, false);
-            }
-            return done(null, user);
-          });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDatabase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to login.");
+        if (err) {
+          return done(err);
         }
-      ));
-}
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+
+  // Incomplete code below
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://hidden-spire-68320.herokuapp.com/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, done) {
+        myDatabase.findOne(
+          { username: profile.username },
+          function (err, user) {
+            console.log(
+              "Github user " + profile.username + " attempted to login."
+            );
+            if (err) {
+              return done(err);
+            } else if (user) {
+              return done(null, user);
+            } else {
+              myDatabase.insertOne(
+                { username: profile.username },
+                (err, doc) => {
+                  if (err) {
+                    return done(err);
+                  } else {
+                    return done(null, doc.ops[0]);
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    )
+  );
+};
--- routes.js
+++ routes.js
@@ -1,75 +1,90 @@
-const passport = require('passport');
-const bcrypt = require('bcrypt');
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 
 module.exports = function (app, myDatabase) {
-    app.route('/')
-    .get((req, res) => {
-      res.render(
-        process.cwd() + '/views/pug/index', {
-          title: 'Connected to database',
-          message: 'Please login',
-          showLogin: true,
-          showRegistration: true
-        }
-      );
+  app.route("/").get((req, res) => {
+    res.render(process.cwd() + "/views/pug/index", {
+      title: "Connected to database",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true,
     });
+  });
 
-  app.route('/login')
-    .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-      res.redirect('/profile')
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + "/views/pug/profile", {
+      username: req.user.username,
     });
+  });
 
-  app.route('/profile')
-    .get(ensureAuthenticated, (req, res) => {
-      res.render(process.cwd() + '/views/pug/profile', {
-        username: req.user.username
-      });
-    });
+  app.route("/logout").get((req, res) => {
+    req.logout;
+    res.redirect("/");
+  });
 
-  app.route('/logout')
-    .get((req, res) => {
-      req.logout;
-      res.redirect('/');
-    });
-
-  app.route('/register')
-    .post((req, res, next) => {
+  app.route("/register").post(
+    (req, res, next) => {
       const hash = bcrypt.hashSync(req.body.password, 12);
 
-      myDatabase.findOne({ username: req.body.username }, function(err, user) {
+      myDatabase.findOne({ username: req.body.username }, function (err, user) {
         if (err) {
           next(err);
         } else if (user) {
-          res.redirect('/')
+          res.redirect("/");
         } else {
-          myDatabase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
-            if (err) {
-              res.redirect('/');
-            } else {
-              // The inserted document is held within
-              // the ops property of the doc
-              next(null, doc.ops[0])
+          myDatabase.insertOne(
+            { username: req.body.username, password: hash },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
             }
-          });
+          );
         }
       });
     },
-      passport.authenticate('local', { failureRedirect: '/' }),
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
+
+  app
+    .route("/auth/github")
+    // .get(passport.authenticate("github"));
+    .get(passport.authenticate("github", { scope: ["user:email"] }));
+
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
       (req, res) => {
-        res.redirect('/profile');
+        res.redirect("/profile");
       }
     );
 
   app.use((req, res) => {
-    res.status(404)
-      .type('text')
-      .send('Not found');
-  })
-}
+    res.status(404).type("text").send("Not found");
+  });
+};
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  res.redirect("/");
 }
